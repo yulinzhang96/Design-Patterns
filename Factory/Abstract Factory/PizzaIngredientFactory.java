e1ae410cca7f5bdae6afe512a789509986617f10
Yulin(Leonard) Zhang <yuliz12@uci.edu>
2024-02-11 11:25:59 -0800

    Update PizzaIngredientFactory.java

--- Factory/Abstract Factory/PizzaIngredientFactory.java
+++ Factory/Abstract Factory/PizzaIngredientFactory.java
@@ -1,3 +1,6 @@
+/**
+ * The Abstract Factory Pattern provides an interface for creating families of related or dependent objects without specifiying their concrete classes.
+ */
 public interface PizzaIngredientFactory {
 
   public Dough createDough();
