a9e2b120d849fc3a4b86d0c85cf93cfc13bdad15
Yulin(Leonard) Zhang <yuliz12@uci.edu>
2024-02-11 10:47:29 -0800

    Create PizzaStore.java

---
+++ Factory/Factory Method/PizzaStore.java
@@ -0,0 +1,19 @@
+public abstract class PizzaStore {
+
+  public Pizza orderPizza(String type) {
+    Pizza pizza;
+
+    pizza = createPizza(type);
+
+    pizza.prepare();
+    pizza.bake();
+    pizza.cut();
+    pizza.box();
+
+    return pizza;
+  }
+
+  protected abstract Pizza createPizza(String type);
+
+  // other methods here
+}
