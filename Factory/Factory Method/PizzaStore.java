f1b7f62e4537fee9716ea5d044365dd7e7c07de8
Yulin(Leonard) Zhang <yuliz12@uci.edu>
2024-02-11 11:04:24 -0800

    Update PizzaStore.java

--- Factory/Factory Method/PizzaStore.java
+++ Factory/Factory Method/PizzaStore.java
@@ -13,6 +13,7 @@ public abstract class PizzaStore {
     return pizza;
   }
 
+  // The Factory Method Pattern defines an interface for creating an object, but lets subclassees decide which class to instantiate. Factory Method lets a class defer instantiation to subclasses.
   protected abstract Pizza createPizza(String type);
 
   // other methods here
