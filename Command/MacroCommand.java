5e2eca1913e36ef59a06e35a192ae2f977517d8b
Yulin(Leonard) Zhang <yuliz12@uci.edu>
2024-02-18 16:13:40 -0800

    Create MacroCommand.java

---
+++ Command/MacroCommand.java
@@ -0,0 +1,13 @@
+public class MacroCommand implements Command {
+  Command[] commands;
+
+  public MacroCommand(Command[] commands) {
+    this.commands = commands;
+  }
+
+  public void execute() {
+    for (int i = 0; i < commands.length; i++) {
+      commands[i].execute();
+    }
+  }
+}
