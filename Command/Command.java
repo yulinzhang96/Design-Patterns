f25e58673cb46bcc76291a04e5ee4e15c3fa7986
Yulin(Leonard) Zhang <yuliz12@uci.edu>
2024-02-18 16:04:31 -0800

    Update Command.java

--- Command/Command.java
+++ Command/Command.java
@@ -1,4 +1,5 @@
 // The Command Pattern encapsulates a request as an object, thereby letting you parameterize other objects with different requests, queue or log requests, and support undoable operations.
 public interface Command 
   public void execute();
+  public void undo();
 }
